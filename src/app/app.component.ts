ea082d350c8722d88ee7137114e45862ab098fb3
tchkoidze <ilia.chkoidze12@gmail.com>
2024-03-28 19:50:35 +0400

    fix:searchbar style and searchbar component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,22 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
   title = 'github-search-app-Angular';
+  isDarkMode: boolean = false;
+
+  moonImg = '/assets/images/moon.svg';
+  sunImg = '/assets/images/sun.svg';
+
+  switchTheme() {
+    const bodyClasses = window.document.body.classList;
+    this.isDarkMode = !this.isDarkMode;
+    if (this.isDarkMode) {
+      bodyClasses.add('dark');
+    } else {
+      bodyClasses.remove('dark');
+    }
+  }
 }
--- src/app/searchbar/searchbar.component.ts
+++ src/app/searchbar/searchbar.component.ts
@@ -1,10 +1,11 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-searchbar',
   templateUrl: './searchbar.component.html',
-  styleUrls: ['./searchbar.component.scss']
+  styleUrls: ['./searchbar.component.scss'],
 })
 export class SearchbarComponent {
-
+  @Input() isDarkMode: boolean = false;
+  @Input() message: boolean = false;
 }
