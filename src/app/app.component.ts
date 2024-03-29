8500d86103bf5eb84527e3f2832b99704d10fac1
tchkoidze <ilia.chkoidze12@gmail.com>
2024-03-29 11:46:21 +0400

    import httpclient modules

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,18 +1,24 @@
-import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'github-search-app-Angular';
   isDarkMode: boolean = false;
+  term: string = 'matthewkk01';
 
   moonImg = '/assets/images/moon.svg';
   sunImg = '/assets/images/sun.svg';
 
   errorMsg: boolean = false;
+
+  constructor(private http: HttpClient) {}
+
+  ngOnInit(): void {}
 
   switchTheme() {
     const bodyClasses = window.document.body.classList;
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,13 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { SearchbarComponent } from './searchbar/searchbar.component';
+
 import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [AppComponent, SearchbarComponent],
-  imports: [BrowserModule, FormsModule],
+  imports: [BrowserModule, FormsModule, HttpClientModule],
   providers: [],
   bootstrap: [AppComponent],
 })
